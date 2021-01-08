1814b571fe184997a226a546f722252ba8609d37
amankumr42 <aman.sinha42@gmail.com>
2021-01-08 06:43:43 +0000

    Streaming insert to big query

--- Google-Cloud/DataFlow/PubSubToBigQueryParseApacheWebserverLogs.py
+++ Google-Cloud/DataFlow/PubSubToBigQueryParseApacheWebserverLogs.py
@@ -19,34 +19,6 @@ import json
 import apache_beam.transforms.window as window
 from datetime import datetime
  
-
-class GroupWindowsIntoBatches(beam.PTransform):
-    """A composite transform that groups Pub/Sub messages based on publish
-    time and outputs a list of dictionaries, where each contains one message
-    and its publish timestamp.
-    """
-
-    def __init__(self, window_size):
-        # Convert minutes into seconds.
-        self.window_size = int(window_size * 60)
-
-    def expand(self, pcoll):
-        return (
-            pcoll
-            # Assigns window info to each Pub/Sub message based on its
-            # publish timestamp.
-            | "Window into Fixed Intervals"
-            >> beam.WindowInto(window.FixedWindows(self.window_size))
-            # Use a dummy key to group the elements in the same window.
-            # Note that all the elements in one window must fit into memory
-            # for this. If the windowed elements do not fit into memory,
-            # please consider using `beam.util.BatchElements`.
-            # https://beam.apache.org/releases/pydoc/current/apache_beam.transforms.util.html#apache_beam.transforms.util.BatchElements
-            | "Add Dummy Key" >> beam.Map(lambda elem: (None, elem))
-            | "Groupby" >> beam.GroupByKey()
-            | "Abandon Dummy Key" >> beam.MapTuple(lambda _, val: val)
-        )
-
 class ParseApacheServerLog(beam.DoFn):
   @classmethod
   def process(self, element):
@@ -117,7 +89,6 @@ def run (input_topic, output_table, window_size=1.0, pipeline_args=None):
         p 
         | "read data from pubsub" >> beam.io.ReadFromPubSub(topic=input_topic).with_output_types(bytes)
         | "lines" >> beam.Map(lambda x : x.decode("utf-8"))
-        | "group messages to window interval" >> GroupWindowsIntoBatches(window_size)
         | "Parse server log data" >> beam.ParDo(ParseApacheServerLog())
         | "Write to Big Query" >> beam.io.WriteToBigQuery(output_table
                                                           ,schema = table_schema
